ebb9a7a4a84caaa0e8589f1d2581e17746a5c2d2
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-12-15 14:40:05 +0600

    mongodb and middlware added

---
+++ index.js
@@ -0,0 +1,56 @@
+const express= require('express')
+
+const app=express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//environment variable
+ require('dotenv').config();
+
+const port=process.env.PORT||3000
+   
+//middleware
+
+   app.use(cors());
+   app.use(express.json());
+
+
+   
+
+     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6pjurty.mongodb.net/?appName=Cluster0`;
+
+     const client = new MongoClient(uri, {
+             serverApi: {
+              version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+                  }
+                  } );
+
+
+       async function run(){
+                         
+                    try{
+                          await client.connect();
+                          await client.db('admin').command({ping:1})
+                          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+                        //   const database=client.db ('Decoration_Booking_DB');
+
+                        //   const databaseCollection= database.collection('Decoration')
+                    }
+
+                    finally{
+
+                    }
+       }           
+        
+     run().catch(console.dir);
+
+app.get('/',(req,res)=>{
+     res.send("Welcome to smart home and decoration booking system")
+})
+
+app.listen(port,()=>{
+      console.log("Port move to",port);
+})
