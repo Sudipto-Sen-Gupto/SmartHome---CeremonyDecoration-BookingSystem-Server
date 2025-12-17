fa186319436c777b7175ce42fda19feb7fc798dc
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-12-18 05:08:06 +0600

    view detail api implement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express')
 
 const app=express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //environment variable
  require('dotenv').config();
 
@@ -63,6 +63,20 @@ const port=process.env.PORT||3000
                             const result=await decorationDataCollection.find().limit(6).toArray();
 
                             res.send(result)
+                          })
+
+
+                          // viewDetail page api
+
+                          app.get('/decorPack/:id',async(req,res)=>{
+                                   
+                                  const id=req.params.id;
+
+                                  const query={_id: new ObjectId(id)};
+
+                                  const result=await decorationDataCollection.findOne(query);
+
+                                  res.send(result);
                           })
                     }
 
